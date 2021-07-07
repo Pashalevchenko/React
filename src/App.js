3748eb28045ebda839b6682d5decbd98a05fe6cc
Pasha <pashalevchenko1995@gmail.com>
2021-07-07 11:13:15 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import './App.css';
+import {useSelector, useDispatch} from "react-redux";
+import {useEffect} from "react";
 
 function App() {
 
+    const fetchPost = async () =>{
+        const data = await (await fetch('https://jsonplaceholder.typicode.com/posts')).json();
+        dispatch({
+            type: 'SET.POSTS',
+            payload: data
+        })
+    }
+    useEffect(()=>{
+        fetchPost()
+    },[])
+    const counter = useSelector(({caunterValue}) => caunterValue)
+    const posts = useSelector(({posts}) => posts)
+    const dispatch = useDispatch()
     return (
         <div>
-
-
+            <h1>{counter}</h1>
+            <button onClick={() => {
+                dispatch({type: 'INC', payload: 4})
+            }
+            }>Increse
+            </button>
+            <button onClick={() => {
+                dispatch({type: 'DEC'})
+            }
+            }>DEC
+            </button>
+            <button onClick={() => {
+                dispatch({type: 'RESET'})
+            }
+            }>RESET
+            </button>
+            {posts.map(post =>(
+                <p key={post.id}>{post.title}</p>
+            ))}
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,43 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+const initialState = {
+    caunterValue: 0,
+    posts: []
+}
+
+const counterReduser = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INC': {
+            return {...state, caunterValue: state.caunterValue + action.payload}
+        }
+        case 'DEC': {
+            return {...state, caunterValue: state.caunterValue - 1}
+        }
+        case 'RESET': {
+            return {...state, caunterValue: 0}
+        }
+        case 'SET.POSTS': {
+            return {...state, posts: action.payload}
+        }
+        default:
+            return state
+    }
+}
+
+const store = createStore(counterReduser)
+console.log(store)
+
 
 
 ReactDOM.render(
     <React.StrictMode>
+        <Provider store={store}>
             <App/>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
