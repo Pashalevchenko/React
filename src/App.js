92dc1e7d72b7cd7987de5024d53d4ce4ed6d7a2d
Pasha <pashalevchenko1995@gmail.com>
2021-07-09 14:09:20 +0300

    redux_2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {useSelector} from "react-redux";
+import {useEffect} from "react";
+
+const createTodoForm= () =>{
+
+}
+
+const  Todos = ()=>{}
 
 function App() {
+  const store = useSelector(store => store);
+  console.log(store);
+  const fetchTodos = async ()=>{
+    const resp = await fetch('http://localhost:8888/get-todos')
+    const data = await resp.json()
+    console.log(data, 'data')
+  }
+
+  useEffect(() =>{
+    fetchTodos()
+  },[])
+
   return (
     <div>
 
---
+++ src/redux/redusers/todos.js
@@ -0,0 +1,13 @@
+const initialState = {
+    todos: []
+}
+
+export const todosReducer = (state = initialState, action)=>{
+    switch (action.type){
+        // case 'INC':{
+        //     return {...state, value: state.value + 1}
+        // }
+        default:
+            return  state
+    }
+}
