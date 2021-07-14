10b065064a09c3abff5b0d6fb947e31bbd18f787
Pasha <pashalevchenko1995@gmail.com>
2021-07-14 10:30:43 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
-import React, {Fragment} from "react";
+import React from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {useEffect, useState} from "react";
-import {todosReducer} from "./redux/redusers/todos";
-import {setLoadingFalse, setLoadingTrue, addTodos, pushTodo, removeTodo } from "./redux/actionCreators";
+import {pushTodo} from "./redux/actionCreators";
 import Todos from "./commponent/todos/Todos";
 import TodosForm from "./commponent/todosForm/TodosForm";
 
@@ -11,26 +9,24 @@ function App() {
     const {todos, todosLoading} = useSelector(store => store.todosReducer);
     const dispach = useDispatch();
 
-    const onTodoCreate = async (title, description) =>{
+    const onTodoCreate = async (title, description) => {
         if (!title || !description) return;
 
-        const resp =await fetch('http://localhost:8888/create-todo', {
+        const resp = await fetch('http://localhost:8888/create-todo', {
             method: "POST",
             body: JSON.stringify({title, description}),
             headers: {
                 'Content-Type': 'application/json'
             }
         })
-        const data = await resp.json()
-        dispach(pushTodo(data))
-
+        const data = await resp.json();
+        dispach(pushTodo(data));
     }
 
     return (
         <div>
             {/*<СreateTodoForm onSubmit={onTodoCreate}/>*/}
             <TodosForm onSubmit={onTodoCreate}/>
-
             <Todos todos={todos} isLoading={todosLoading}/>
         </div>
     );
